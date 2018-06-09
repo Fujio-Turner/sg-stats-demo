adf63a81516541ad828067ba5aac80dcddbdd7ef
Fujio Turner <fujio-turner@users.noreply.github.com>
2018-06-09 13:39:21 -0300

    update print()

--- sg-stats-demo.py
+++ sg-stats-demo.py
@@ -72,7 +72,7 @@ class S(BaseHTTPRequestHandler):
         
 
     def pullSGStats(self):
-        url = "http://localhost:4985/_expvar"
+        url = "http://10.2.21.113:4985/_expvar"
         if self.debug == True:
             print("DEBUG: ", url)
 
@@ -92,7 +92,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
+    print('Starting httpd...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
